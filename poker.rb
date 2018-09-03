6809cd42df2e03129aab8af91fe19645f7b8ee14
Josh Varney <joshcainvarney@gmail.com>
2018-09-02 22:06:46 -0400

    finished four of a kind testing

--- poker.rb
+++ poker.rb
@@ -23,35 +23,116 @@ def card_values()
 	end
 	value_hash			
 end
-def draw_hands()
-	black = ['Black:']
-	white = ['White:']
-	hands = []
-	shuffle = deck().shuffle!
-	counter = 0 
-	shuffle.each_with_index do |deal, index|
-		unless counter < 3	|| counter > 12
-			if index.even? == false
-				black << deal
-			else
-				white << deal
+# def draw_hands()
+# 	black = ['Black:']
+# 	white = ['White:']
+# 	hands = []
+# 	shuffle = deck().shuffle!
+# 	counter = 0 
+# 	shuffle.each_with_index do |deal, index|
+# 		unless counter < 3	|| counter > 12
+# 			if index.even? == false
+# 				black << deal
+# 			else
+# 				white << deal
+# 			end
+# 		end
+# 		counter += 1	
+# 	end
+# 	hands << black 
+# 	hands << white 				
+# end
+def check_hands()
+	# check_hands = draw_hands()
+	check_hands = [["Black:", "JD", "JC", "JH", "JS", "7D"], ["White:", "3C", "AD", "AS", "AH", "AC"]]
+
+
+end	
+def replace_cards()
+	hand_values = check_hands()
+	card_values.each do |key, value|
+		counter = 0
+		hand_values[0].each { |stuff|
+			if stuff == key
+				hand_values[0][counter] = value				
+			end
+			counter += 1
+		}
+	end
+	card_values.each do |key, value|
+		counter = 0
+		hand_values[1].each { |stuff|
+			if stuff == key
+				hand_values[1][counter] = value				
+			end
+			counter += 1
+		}
+	end
+	hand_values[0][1..5] = hand_values[0][1..5].sort
+	hand_values[1][1..5] = hand_values[1][1..5].sort
+	hand_values
+end	
+def checking_flush()
+	checking_flush = check_hands	
+	suit_check = []
+	compare_suits = []
+	matching_suits = ["CCCCC", "DDDDD", "HHHHH", "SSSSS"]
+		check_hands().each do |hand|
+			hand.each do |elements|
+				unless elements == "Black:" || elements == "White:"
+					suits = elements.split('')
+					suit_check << suits[1]
+				end 
 			end
 		end
-		counter += 1	
-	end
-	hands << black 
-	hands << white 				
+	compare_suits << suit_check[0..4].join  
+	compare_suits << suit_check[5..9].join
+		compare_suits.each_with_index do |hand, index|
+			matching_suits.each do |suits|
+				if hand == suits && index == 0
+					checking_flush[0][6] = "flush"
+				elsif hand == suits && index == 1
+					checking_flush[1][6] = "flush"
+				end				 	 
+			end
+		end
+	checking_flush	 
 end
-def check_hands()
-	check = draw_hands()
-end	
-	p check_hands
-# def checking_flush()
-	
-# 	suit_check = []
-# 	value_check = []
-# 	matching_suits = ["CCCCC", "DDDDD", "HHHHH", "SSSSS"]
-
-# 	straight_flush
-# end
-# p checking_flush
+def checking_straight()
+	straight = replace_cards()
+	checking_straight = check_hands
+	check01 = straight[0][5] - straight[0][4]
+	check02 = straight[0][4] - straight[0][3]
+	check03 = straight[0][3] - straight[0][2]
+	check04 = straight[0][2] - straight[0][1]
+	if check01 == 1 && check02 == 1 && check03 == 1 && check04 == 1
+		checking_straight[0][6] = "straight"
+	end	
+	check11 = straight[1][5] - straight[1][4]
+	check12 = straight[1][4] - straight[1][3]
+	check13 = straight[1][3] - straight[1][2]
+	check14 = straight[1][2] - straight[1][1]
+	if check11 == 1 && check12 == 1 && check13 == 1 && check14 == 1
+		checking_straight[1][6] = "straight"
+	end
+	checking_straight
+end
+def checking_four()
+	checking_four = check_hands
+	four = replace_cards
+	counter = 0
+	four.each do |x|
+		if x[5] == x[4] && x[4] == x[3] && x[3] == x[2] && counter == 0
+			checking_four[0][6] = "four of a kind"
+		elsif x[1] == x[2] && x[2] == x[3] && x[3] == x[4] && counter == 0	
+			checking_four[0][6] = "four of a kind"
+		elsif x[5] == x[4] && x[4] == x[3] && x[3] == x[2] && counter == 1
+			checking_four[1][6] = "four of a kind"
+		elsif x[1] == x[2] && x[2] == x[3] && x[3] == x[4] && counter == 1	
+			checking_four[1][6] = "four of a kind"
+		end
+		counter += 1		
+	end
+	checking_four	
+end
+p checking_four
--- poker_test.rb
+++ poker_test.rb
@@ -2,46 +2,76 @@ require 'minitest/autorun'
 require_relative 'poker.rb'
 class Poker_test < Minitest::Test
 
-	def test_deck_returns_array
-		assert_equal(Array, deck().class)
-	end
-	def test_that_do_loop_combines_correctly
-		assert_equal('2C', deck()[0])
-	end
-	def test_that_deck_contains_correct_amount
-		assert_equal(52, deck().count)
-	end
-	def test_that_do_loop_keeps_working
-		assert_equal('2D', deck()[13])
-	end
-	def test_that_draw_hands_returns_array
-		assert_equal(Array, draw_hands().class)
-	end
-	def test_that_shuffle_works
-		refute_equal('2C', draw_hands()[0])
-	end
-	def test_that_dealing_cards_works
-		assert_equal(6, draw_hands()[0].count)
-	end
-	def test_that_players_line_up_right
-		assert_equal('Black:', draw_hands()[0][0])
-	end
-	def test_that_players_line_up_right
-		assert_equal('White:', draw_hands()[1][0])
-	end
-	def test_that_card_values_is_a_hash
-		assert_equal(Hash, card_values().class)
-	end
-	def test_that_card_value_has_the_right_amount
-		assert_equal(52, card_values().count)
-	end
-	def test_that_values_are_lining_up_correct
-		assert_equal(2, card_values()["2D"])
-	end
-	def test_that_values_continue_lining_up
-		assert_equal(14, card_values()["AS"])
-	end
-	# def test_that_can_put_suits_in_string_to_compare
-	# 	assert_equal(String, straight_flush().class)
+	# def test_deck_returns_array
+	# 	assert_equal(Array, deck().class)
 	# end
+	# def test_that_do_loop_combines_correctly
+	# 	assert_equal('2C', deck()[0])
+	# end
+	# def test_that_deck_contains_correct_amount
+	# 	assert_equal(52, deck().count)
+	# end
+	# def test_that_do_loop_keeps_working
+	# 	assert_equal('2D', deck()[13])
+	# end
+	# def test_that_draw_hands_returns_array
+	# 	assert_equal(Array, draw_hands().class)
+	# end
+	# def test_that_shuffle_works
+	# 	refute_equal('2C', draw_hands()[0])
+	# end
+	# def test_that_dealing_cards_works
+	# 	assert_equal(6, draw_hands()[0].count)
+	# end
+	# def test_that_players_line_up_right
+	# 	assert_equal('Black:', draw_hands()[0][0])
+	# end
+	# def test_that_players_line_up_right
+	# 	assert_equal('White:', draw_hands()[1][0])
+	# end
+	# def test_that_card_values_is_a_hash
+	# 	assert_equal(Hash, card_values().class)
+	# end
+	# def test_that_card_value_has_the_right_amount
+	# 	assert_equal(52, card_values().count)
+	# end
+	# def test_that_values_are_lining_up_correct
+	# 	assert_equal(2, card_values()["2D"])
+	# end
+	# def test_that_values_continue_lining_up
+	# 	assert_equal(14, card_values()["AS"])
+	# end
+	# def test_that_can_put_suits_in_string_to_compare
+	# 	assert_equal(2, checking_flush().count)
+	# end
+	# def test_that_checking_flush_array_does_not_conain_player_name
+	# 	assert_equal(false, checking_flush().include?("Black:"))
+	# end
+	# def test_that_adds_flush_into_array_correctly
+	# 	assert_equal([["Black:", "2H", "5H", "KH", "9H", "TH", "flush"], ["White:", "3H", "QS", "JH", "TD", "7C"]], checking_flush())
+	# end
+	# def test_that_adds_flush_to_white
+	# 	assert_equal([["Black:", "2D", "5H", "KS", "9C", "TH"], ["White:", "3S", "QS", "JS", "TS", "7S", "flush"]],checking_flush())
+	# end
+	# def test_that_adds_flush_to_both_players
+	# 	assert_equal([["Black:", "2D", "5D", "KD", "9D", "TD", "flush"], ["White:", "3C", "QC", "JC", "TC", "7C", "flush"]], checking_flush())	
+	# end
+	# def test_that_can_replace_card_with_the_value_and_add_straight_to_black
+	# 	assert_equal([["Black:", "QD", "JD", "KD", "9D", "TD", "straight"], ["White:", "3C", "QC", "JC", "TC", "7C"]], checking_straight())
+	# end
+	# def test_that_can_replace_card_with_the_value_and_add_straight_to_white
+	# 	assert_equal([["Black:", "2D", "5D", "KD", "9D", "TD"], ["White:", "3C", "5C", "6C", "4C", "2C", "straight"]], checking_straight())
+	# end
+	# def test_that_can_add_straight_to_both_players
+	# 	assert_equal([["Black:", "6D", "5D", "8D", "9D", "7D", "straight"], ["White:", "3C", "5C", "6C", "4C", "2C", "straight"]], checking_straight())		
+	# end
+	# def test_that_four_of_a_kind_works_for_black
+	# 	assert_equal([["Black:", "6D", "6C", "6H", "6S", "7D", "four of a kind"], ["White:", "3C", "5C", "6C", "4C", "2C"]], checking_four())
+	# end
+	# def test_that_four_works_for_white
+	# 	assert_equal([["Black:", "6D", "6C", "6H", "7S", "7D"], ["White:", "3C", "5D", "5S", "5H", "5C", "four of a kind"]], checking_four())
+	# end
+	def test_that_four_works_for_both
+		assert_equal([["Black:", "JD", "JC", "JH", "JS", "7D", "four of a kind"], ["White:", "3C", "AD", "AS", "AH", "AC", "four of a kind"]], checking_four())
+	end
 end
